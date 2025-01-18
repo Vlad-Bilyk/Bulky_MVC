e254ee7699d37c870da9d00746abe87b4e7fcf55
Vladyslav Bilyk <bilyk.vlad16@gmail.com>
2025-01-18 12:54:18 +0200

    Add Company CRUD Operations

---
+++ BulkyBook.DataAccess/Repository/CompanyRepository.cs
@@ -0,0 +1,35 @@
+﻿using BulkyBook.DataAccess.Data;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BulkyBook.DataAccess.Repository
+{
+    public class CompanyRepository : Repository<Company>, ICompanyRepository
+    {
+        private readonly ApplicationDbContext _db;
+
+        public CompanyRepository(ApplicationDbContext db) : base(db)
+        { 
+            _db = db;
+        }
+
+        public void Update(Company obj)
+        {
+            var objFromDb = _db.Companies.FirstOrDefault(u => u.Id == obj.Id);
+            if (objFromDb != null)
+            {
+                objFromDb.Name = obj.Name;
+                objFromDb.PhoneNumber = obj.PhoneNumber;
+                objFromDb.City = obj.City;
+                objFromDb.StreetAddress = obj.StreetAddress;
+                objFromDb.State = obj.State;
+                objFromDb.PostalCode = obj.PostalCode;
+            }
+        }
+    }
+}
